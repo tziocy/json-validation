f1b271878cd231d1f00e094ddde11fa1bb55cd42
tziocy <tzio@epicode.gr>
2018-01-26 11:17:28 +0200

    Add Error Handling

--- index.js
+++ index.js
@@ -3,18 +3,26 @@ const express = require('express');
 const Ajv = require('ajv');
 
 const app = express();
-const schema = JSON.parse(fs.readFileSync('./public/datamodelSchema.json', 'utf8'));
-const data = JSON.parse(fs.readFileSync('./public/datamodel.json', 'utf8'));
+
+var validateData = () => {
+    try {
+        var schema = JSON.parse(fs.readFileSync('./public/datamodelSchema.json', 'utf8'));
+        var data = JSON.parse(fs.readFileSync('./public/datamodel.json', 'utf8'));
+        var validate = ajv.compile(schema);
+        var valid = validate(data);
+        if (!valid) return { valid : false, errors: validate.errors };
+        return { valid : true };
+    }catch (e) {
+        console.log(e.message);
+        return { 
+            valid : false,
+            errors: e.message
+        };
+    }
+}
 
 var ajv = new Ajv({ schemaId: 'auto', allErrors: true });
 ajv.addMetaSchema(require('ajv/lib/refs/json-schema-draft-04.json'));
-
-var validateData = () => {
-    var validate = ajv.compile(schema);
-    var valid = validate(data);
-    if (!valid) return { valid : false, errors: validate.errors }
-    return { valid : true }
-}
 
 app.get('/datamodel/check', (req, res) => res.send( validateData() ));
 app.listen(3000, () => console.log( validateData() ));
